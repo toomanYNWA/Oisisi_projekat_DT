5a91a765422595e4ebcb448c93ffcd7be154d976
Dragana Kucanski <kucanski.ra164.2019@uns.ac.rs>
2021-11-16 18:48:06 +0100

    Main Frame created

--- src/main/MyApp.java
+++ src/main/MyApp.java
@@ -1,10 +1,24 @@
 package main;
 
+import java.awt.EventQueue;
+
+import view.MainFrame;
 
 public class MyApp {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("Proba");
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					MainFrame frame = new MainFrame();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
 	}
 
 }
--- src/view/MainFrame.java
+++ src/view/MainFrame.java
@@ -1,5 +1,38 @@
 package view;
 
-public class MainFrame {
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Toolkit;
 
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTabbedPane;
+
+public class MainFrame extends JFrame {
+	
+	private JPanel contentPane;
+	
+	public MainFrame() {
+		
+		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+		int W = (int)screenSize.getWidth();
+		int H = (int)screenSize.getHeight();
+		int newW =  (int) (W *0.75);
+		int newH =  (int) (H *0.75);
+		
+		setSize(newW ,newH);
+		setLocationRelativeTo(null);
+		setResizable(false);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		setTitle("Studentska služba");
+		
+		contentPane = new JPanel();
+		contentPane.setBackground(Color.WHITE);
+		setContentPane(contentPane);
+		
+		
+		
+	}
+	
 }
