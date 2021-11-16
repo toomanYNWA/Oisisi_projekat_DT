85c0fac958998fed046f89bf347bc92e0c2cd4b6
Dragana Kucanski <kucanski.ra164.2019@uns.ac.rs>
2021-11-16 21:46:00 +0100

    Bugs fixed

--- src/view/ToolBar.java
+++ src/view/ToolBar.java
@@ -1,7 +1,13 @@
 package view;
 
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
+
 import javax.swing.Box;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JTextField;
 import javax.swing.JToolBar;
 import javax.swing.SwingConstants;
 
@@ -13,23 +19,36 @@ public class ToolBar extends JToolBar{
 		
 		JButton btnCreate = new JButton();
 		btnCreate.setToolTipText("Create");
-		//btnCreate.setIcon(new ImageIcon("");
+		btnCreate.setIcon(new ImageIcon("icons/pluss.png"));
 		add(btnCreate);
+		
+		addSeparator();
 		
 		JButton btnOpen = new JButton();
 		btnOpen.setToolTipText("Open");
-		//btnOpen.setIcon(new ImageIcon("");
+		btnOpen.setIcon(new ImageIcon("icons/edit.png"));
 		add(btnOpen);
+		
+		addSeparator();
 		
 		JButton btnDelete = new JButton();
 		btnDelete.setToolTipText("Delete");
-		//btnDelete.setIcon(new ImageIcon("");
+		btnDelete.setIcon(new ImageIcon("icons/trash.png"));
 		add(btnDelete);
 		
+		addSeparator();
+		
+		
+		
+		JTextField search = new JTextField();
+		search.setPreferredSize(new Dimension(170,35));
+		add(search);
+		
 		add(Box.createHorizontalGlue());
+		
 		JButton btnSearch = new JButton();
 		btnSearch.setToolTipText("Search");
-		//btnDelete.setIcon(new ImageIcon("");
+		btnSearch.setIcon(new ImageIcon("icons/searchh.png"));
 		add(btnSearch);
 		
 		
